ead4fada98496f75f5d231aaca687c4259d679cc
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-05-13 10:30:03 +1000

    Initial commit

---
+++ Assets/Scripts/CustomisationSet.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CustomisationSet : MonoBehaviour
+{
+
+    #region Variables
+    [Header("TextureList")]
+    // Texture2D List for skin, hair, mouth, eyes, clothes, armour
+    public List<Texture2D> skin = new List<Texture2D>();
+    public List<Texture2D> hair = new List<Texture2D>();
+    public List<Texture2D> mouth = new List<Texture2D>();
+    public List<Texture2D> eyes = new List<Texture2D>();
+    public List<Texture2D> clothes = new List<Texture2D>();
+    public List<Texture2D> armour = new List<Texture2D>();
+    [Header("Index")]
+    public int skinIndex;
+    public int hairIndex, mouthIndex, eyesIndex, clothesIndex, armourIndex;
+    [Header("Renderer")]
+    public Renderer character;
+    [Header("Max Index")]
+    public int skinMax;
+    public int hairmax, mouthMax, eyesMax, clothesmax, armourMax;
+
+    [Header("Character Name")]
+    public string charName = "Adventurer";
+    [Header("Stats")]
+    // Base stats for player
+    public string[] statArray = new string[6];
+    public int[] stats = new int[6];
+    public int[] tempStats = new int[6];
+    public int points = 10;
+    public CharacterClass charClass = CharacterClass.Barbarian;
+    public string[] selectedClass = new string[8];
+    public int selectedIndex = 0;
+
+    #endregion
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+
+    void Update()
+    {
+
+    }
+    #region CharacterClass
+    public enum CharacterClass
+    {
+        Barbarian,
+        Bard,
+        Druid,
+        Monk,
+        Paladin,
+        Ranger,
+        Sorcerer,
+        Warlock
+    }
+}
